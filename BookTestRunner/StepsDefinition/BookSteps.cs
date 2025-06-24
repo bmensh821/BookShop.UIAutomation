d5bc0980d6bf810654750906c62cbc10358b9c11
bmensh821 <foam.mensh@gmail.com>
2025-06-24 15:01:45 -0400

    Add project files.

---
+++ BookTestRunner/StepsDefinition/BookSteps.cs
@@ -0,0 +1,37 @@
+﻿using Reqnroll;
+
+namespace BookTestRunner.StepsDefinition;
+
+public sealed class BookSteps
+{
+    [Given("I go to the Books page")]
+    public void GivenIGoToTheBooksPage()
+    {
+        throw new PendingStepException();
+    }
+
+    [When("I select {string} from the side menu")]
+    public void WhenISelectFromTheSideMenu(string books)
+    {
+        throw new PendingStepException();
+    }
+
+    [Then(@"I should see all Books available with the total of ""(.*)"" results")]
+    public void ThenIShouldSeeAllBooksAvailableWithTheTotalOfResults(string p0)
+    {
+        throw new PendingStepException();
+    }
+
+    [When(@"I locate book named ""(.*)""")]
+    public void WhenILocateBookNamed(string p0)
+    {
+        throw new PendingStepException();
+    }
+
+    [Then(@"I should see the book price of ""(.*)"" and I can add it to the basket")]
+    public void ThenIShouldSeeTheBookPriceOfAndICanAddItToTheBasket(string p0)
+    {
+        throw new PendingStepException();
+    }
+
+}
---
+++ BookTestRunner/TestHooks.cs
@@ -0,0 +1,24 @@
+﻿using Reqnroll;
+using Atata;
+
+namespace BookTestRunner;
+
+[Binding]
+public class TestHooks
+{
+    [BeforeScenario]
+    public void SetUp()
+    {
+        AtataContext.Configure()
+            .UseChrome()
+            .UseNUnitTestName()
+            .LogConsumers.AddNUnitTestContext()
+            .Build();
+    }
+
+    [AfterScenario]
+    public void TearDown()
+    {
+        AtataContext.Current?.Dispose();
+    }
+}
